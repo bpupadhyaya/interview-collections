740f2dbf220146c56af534cfa8727a7ce9fecec7
Bhim Upadhyaya <bpupadhyaya@gmail.com>
2016-03-05 22:19:03 -0800

    Initial project structure.

---
+++ interview-collections-algo/build.sbt
@@ -0,0 +1,6 @@
+name := "interview-collections-algo"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+    
---
+++ interview-collections-algo/src/main/java/com/equalinformation/interview/algo/java/Temp.java
@@ -0,0 +1,7 @@
+package com.equalinformation.interview.algo.java;
+
+/**
+ * Created by bpupadhyaya on 3/5/16.
+ */
+public class Temp {
+}
