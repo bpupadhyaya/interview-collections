01630388a8542ac165656bb94bdd425d6ab06085
Bhim Upadhyaya <bpupadhyaya@gmail.com>
2016-03-06 21:29:24 -0800

    Linked list and its application (stack and queue).

---
+++ interview-collections-algo/src/main/java/com/equalinformation/interview/algo/java/linked_list/LinkedList.java
@@ -0,0 +1,15 @@
+package com.equalinformation.interview.algo.java.linked_list;
+
+/**
+ * Linked List: A node that has a value and a link to next node.
+ * Created by bpupadhyaya on 3/6/16.
+ */
+public class LinkedList {
+    int val;
+    LinkedList next;
+
+    LinkedList(int x) {
+        val = x;
+        next = null;
+    }
+}
---
+++ interview-collections-algo/src/main/java/com/equalinformation/interview/algo/java/linked_list/Node.java
@@ -0,0 +1,16 @@
+package com.equalinformation.interview.algo.java.linked_list;
+
+/**
+ * Node is a linked list implementation.
+ *
+ * Created by bpupadhyaya on 3/6/16.
+ */
+public class Node {
+    int val;
+    Node next;
+
+    Node(int val) {
+        this.val = val;
+        next = null;
+    }
+}
