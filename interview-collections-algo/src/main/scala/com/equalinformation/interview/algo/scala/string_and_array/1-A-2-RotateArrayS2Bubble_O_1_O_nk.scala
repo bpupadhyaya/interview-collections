34fda2a8a6a4123a2d666c2a49f9cb19486e7f52
bpupadhyaya <bpupadhyaya@gmail.com>
2016-06-12 16:34:13 -0700

    Scala version of rotate array, bubble rotate.

--- interview-collections-algo/src/main/scala/com/equalinformation/interview/algo/scala/string_and_array/1-A-2-RotateArrayS2Bubble_O_1_O_nk.scala
+++ interview-collections-algo/src/main/scala/com/equalinformation/interview/algo/scala/string_and_array/1-A-2-RotateArrayS2Bubble_O_1_O_nk.scala
@@ -6,11 +6,35 @@ package com.equalinformation.interview.algo.scala.string_and_array
 
 object RotateArrayS2Bubble_O_1_O_nkTest {
   def main(args: Array[String]): Unit = {
-    //TODO
+    val soln = new RotateArrayS2Bubble_O_1_O_nk
+    val givenArray = Array(1,2,3,4,5)
+    val k = 2
+    soln.rotate(givenArray, k)
   }
 
 }
 
 class RotateArrayS2Bubble_O_1_O_nk {
-  //TODO
+  def rotate(nums: Array[Int], k: Int): Unit = {
+
+    val tempNums = nums.clone()
+
+    if(tempNums == null || k < 0) {
+      throw new IllegalArgumentException("Invalid arguments")
+    }
+
+    for(i <- 0 to k - 1) {
+      for(j <- tempNums.length -1 to 1 by -1) {
+        val temp = tempNums(j);
+        tempNums(j) = tempNums(j-1)
+        tempNums(j-1) = temp
+
+      }
+    }
+
+    println("Rotated array: ")
+    for(element <- tempNums) {
+      print(element+", ")
+    }
+  }
 }
