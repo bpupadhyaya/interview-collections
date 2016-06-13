7f0c1c0e56ac2934bcba5c8d9cc7210ea38257e3
bpupadhyaya <bpupadhyaya@gmail.com>
2016-06-12 19:15:05 -0700

    Scala version of reverse polish notation evaluation, naive approach.

--- interview-collections-algo/src/main/scala/com/equalinformation/interview/algo/scala/string_and_array/2-A-1-ReversePolishNotationEvaluation_Naive.scala
+++ interview-collections-algo/src/main/scala/com/equalinformation/interview/algo/scala/string_and_array/2-A-1-ReversePolishNotationEvaluation_Naive.scala
@@ -1,13 +1,42 @@
 package com.equalinformation.interview.algo.scala.string_and_array
 
+import java.util;
+
 /**
   * Created by bpupadhyaya on 6/12/16.
   */
 
-object ReversePolishNotationEvaluation_NaiveTest {
-  //TODO
+object ReversePolishNotationEvaluation_NaiveTest extends App {
+  val soln = new ReversePolishNotationEvaluation_Naive
+  val tokens = Array("2","1","+","3","*")
+  println("Result: " + soln.evalRPN(tokens))
 }
 
 class ReversePolishNotationEvaluation_Naive {
-  //TODO
+
+  def evalRPN(tokens: Array[String]): Int = {
+    var returnValue = 0
+    val operators = "+-*/"
+
+    val stack = new util.Stack[String]()
+    for(t <- tokens) {
+      if(!operators.contains(t)) {
+        stack.push(t)
+      } else {
+        val a = Integer.valueOf(stack.pop())
+        val b = Integer.valueOf(stack.pop())
+
+        t match {
+          case "+" => stack.push(String.valueOf(a + b))
+          case "-" => stack.push(String.valueOf(b - a))
+          case "*" => stack.push(String.valueOf(a * b))
+          case "/" => stack.push(String.valueOf(b / a))
+        }
+      }
+    }
+
+    returnValue = Integer.valueOf(stack.pop())
+
+    returnValue
+  }
 }
