afd3f3facc48a74a9764c6f3a9c0a54bcecfca62
bpupadhyaya <bpupadhyaya@gmail.com>
2016-06-12 19:57:19 -0700

    Scala version of reverse polish notation evaluation, accepted solution.

--- interview-collections-algo/src/main/scala/com/equalinformation/interview/algo/scala/string_and_array/2-A-2-ReversePolishNotationEvaluation_AcceptedSoln.scala
+++ interview-collections-algo/src/main/scala/com/equalinformation/interview/algo/scala/string_and_array/2-A-2-ReversePolishNotationEvaluation_AcceptedSoln.scala
@@ -1,13 +1,45 @@
 package com.equalinformation.interview.algo.scala.string_and_array
 
+  import java.util.Stack
 /**
   * Created by bpupadhyaya on 6/12/16.
   */
 
 object ReversePolishNotationEvaluation_AcceptedSolnTest {
-  //TODO
+  def main(args: Array[String]): Unit = {
+    val soln = new ReversePolishNotationEvaluation_AcceptedSoln
+    val tokens = Array("2","1","+","3","*")
+    println("Result: " + soln.evalRPN(tokens))
+  }
 }
 
 class ReversePolishNotationEvaluation_AcceptedSoln {
-  //TODO
+
+  def evalRPN(tokens: Array[String]): Int = {
+    var returnValue = 0;
+
+    val operators = "+-*/"
+
+    val stack = new Stack[String]()
+    for(t <- tokens) {
+      if(!operators.contains(t)) {
+        stack.push(t)
+      } else {
+        val a = Integer.valueOf(stack.pop())
+        val b = Integer.valueOf(stack.pop())
+        val index = operators.indexOf(t)
+
+        index match {
+          case 0 => stack.push(String.valueOf(a + b))
+          case 1 => stack.push(String.valueOf(b - a))
+          case 2 => stack.push(String.valueOf(a * b))
+          case 3 => stack.push(String.valueOf(b / a))
+        }
+      }
+    }
+
+    returnValue = Integer.valueOf(stack.pop())
+
+    returnValue
+  }
 }
