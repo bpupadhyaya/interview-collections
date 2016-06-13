daaee2691841ddd9edb46d0968c7f7b702843aa1
bpupadhyaya <bpupadhyaya@gmail.com>
2016-06-12 17:04:02 -0700

    Scala version of rotate array, reversal rotate.

--- interview-collections-algo/src/main/scala/com/equalinformation/interview/algo/scala/string_and_array/1-A-3-RotateArrayS3Reversal_O_1_O_n.scala
+++ interview-collections-algo/src/main/scala/com/equalinformation/interview/algo/scala/string_and_array/1-A-3-RotateArrayS3Reversal_O_1_O_n.scala
@@ -5,10 +5,44 @@ package com.equalinformation.interview.algo.scala.string_and_array
   */
 
 object RotateArrayS3Reversal_O_1_O_nTest {
-  //TODO
+  def main(args: Array[String]): Unit = {
+    val soln = new RotateArrayS3Reversal_O_1_O_n
+    val givenArray = Array(1,2,3,4,5)
+    val k = 2
+    soln.rotate(givenArray,k)
+  }
 }
 
 class RotateArrayS3Reversal_O_1_O_n {
-  //TODO
+
+  def rotate(nums: Array[Int], k: Int): Unit = {
+    var kTemp = k
+    kTemp = kTemp % nums.length
+
+    val a = nums.length - k
+
+    reverse(nums, 0, a - 1)
+    reverse(nums, a, nums.length - 1)
+    reverse(nums, 0, nums.length -1)
+
+    println("Rotated array: ")
+    for(element <- nums) {
+      print(element + ", ")
+    }
+  }
+
+  def reverse(nums: Array[Int], left: Int, right: Int): Unit = {
+    var l = left; var r = right
+    if(nums == null || nums.length ==1)
+      return
+
+    while(l < r) {
+      val temp = nums(l)
+      nums(l) = nums(r)
+      nums(r) = temp
+      l += 1
+      r -= 1
+    }
+  }
 }
 
